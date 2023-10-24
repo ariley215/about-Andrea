a36ba3da00f5486bc6ca197bf23fc963411471ae
Andrea Riley <andreariley@me.com>
2023-10-24 18:40:23 -0400

    got some loose structure

---
+++ js/app.js
@@ -0,0 +1,14 @@
+'use strict'
+
+
+
+// let usersName = prompt('Hi! What is your name?')
+// console.log(usersName);
+
+// let MyNameIs = usersName();
+
+// console.log(MyNameIs);
+
+// function greet(){
+//   document.write('Hey' + MyNameIs + ', thanks for coming to get to know me. ');
+// }
