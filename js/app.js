94bffe8050e788a486370d2c9b5018203b2e56cb
Andrea Riley <andreariley@me.com>
2023-10-24 21:38:46 -0400

    adding more

--- js/app.js
+++ js/app.js
@@ -1,14 +1,53 @@
 'use strict'
 
+// function askYesNoQuestion(question, correctAnswer) {
+//   const userInput = prompt(question).toLowerCase(); // Normalize user input to lowercase
+//   if (userInput === 'y' || userInput === 'yes') {
+//     if (correctAnswer === 'yes') {
+//       alert('Correct! You answered "Yes" or "Y".');
+//     } else {
+//       alert('Incorrect. The correct answer was "No".');
+//     }
+//   } else if (userInput === 'n' || userInput === 'no') {
+//     if (correctAnswer === 'no') {
+//       alert('Correct! You answered "No" or "N".');
+//     } else {
+//       alert('Incorrect. The correct answer was "Yes".');
+//     }
+//   } else {
+//     alert('Invalid input. Please answer with "Yes" or "No" (case-insensitive).');
+//   }
+// }
+
+// askYesNoQuestion('Is the sky blue?', 'yes');
+// askYesNoQuestion('Is water wet?', 'no');
+// askYesNoQuestion('Are you human?', 'yes');
+// askYesNoQuestion('Is JavaScript a programming language?', 'yes');
+// askYesNoQuestion('Can a duck swim?', 'yes');
+
+
 
 
 // let usersName = prompt('Hi! What is your name?')
 // console.log(usersName);
 
-// let MyNameIs = usersName();
+// let MyNameIs = (usersName);
 
 // console.log(MyNameIs);
 
-// function greet(){
-//   document.write('Hey' + MyNameIs + ', thanks for coming to get to know me. ');
-// }
+
+function FirstYoN() {
+  let vacay = prompt('Do you like vacations?');
+  vacay = vacay.toLowercase();
+  console.log(vacay);
+
+
+  if (vacay == "yes") {
+    console.log(vacay);
+  }   else if (vacay == "no") {
+    console.log(vacay);
+  }    else {
+    console.log(vacay);
+
+  }
+}
