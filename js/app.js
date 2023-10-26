a5e4db4dfd41fb50990d2a630653f8489016c48a
Gabriel Urquidez <grurquidez@gmail.com>
2023-10-26 17:03:52 -0500

    update andrea

--- js/app.js
+++ js/app.js
@@ -1,171 +1,78 @@
 'use strict';
 
 function nameIs() {
-  const usersName = prompt('Who are you?');
-
-  return usersName;
+  return prompt('Who are you?');
 }
 
 let HiIAm = nameIs();
-console.log('HiIAm');
+console.log(`Hi ${HiIAm}`);
 
 function greeting() {
-  document.write('Welcome ' + HiIAm + ' stay and hang out for a while.');
+  document.write(`Welcome ${HiIAm}, stay and hang out for a while.`);
 }
 
+function askYesOrNoQuestion(question, positiveResponse, negativeResponse) {
+  let answer = prompt(question).toLowerCase();
 
-function firstYoN() {
-  let vacay = prompt('Do you like vacations?');
-  vacay = vacay.toLowerCase();
-  console.log(vacay);
-
-  if (vacay === 'yes') {
-    alert('They are the best!');
-    //   console.log(vacay);
-  }
-  else if (vacay === 'no') {
-    alert('Why not?');
-    // console.log(vacay);
-  }
-  else {
-    alert('Sounds like you need one');
-    // console.log(vacay);
-
+  if (answer === 'yes') {
+    alert(positiveResponse);
+  } else if (answer === 'no') {
+    alert(negativeResponse);
+  } else {
+    alert('Interesting answer!');
   }
 }
-firstYoN();
 
+askYesOrNoQuestion('Do you like vacations?', 'They are the best!', 'Why not?');
+askYesOrNoQuestion('Do you like the color orange?', 'Its my favorite!', 'You probably like blue');
+askYesOrNoQuestion('Is water wet?', 'Glad you are paying attention', 'hmmmm... not so sure about that one.');
+askYesOrNoQuestion('Have you ever gotten poison ivy?', 'That must have sucked!', 'Same, we are so lucky');
+askYesOrNoQuestion('Are you ready?', "LET'S GO!", 'You better get ready');
 
-function secondYoN() {
-  let color = prompt('Do you like the color orange?');
-  color = color.toLowerCase();
-  console.log(color);
-
-  if (color === 'yes') {
-    alert('Its my favorite!');
-    //   console.log(color);
-  }
-  else if (color === 'no') {
-    alert('You probably like blue');
-    // console.log(color);
-  }
-  else {
-    alert('I like that one too');
-    // console.log(color);
-
-  }
-}
-secondYoN();
-
-
-function thirdYoN() {
-  let water = prompt('Is water wet?');
-  water = water.toLowerCase();
-  console.log(water);
-
-  if (water === 'yes') {
-    alert('Glad you are paying attention');
-    //   console.log(water);
-  }
-  else if (water === 'no') {
-    alert('hmmmm... not so sure about that one.');
-    // console.log(water);
-  }
-  else {
-    alert('You must be thinking of something else');
-    // console.log(water);
-
-  }
-}
-thirdYoN();
-
-
-function fourthYoN() {
-  let poisonIvy = prompt('Have you ever gotten poison ivy?');
-  poisonIvy = poisonIvy.toLowerCase();
-  console.log(poisonIvy);
-
-  if (poisonIvy === 'yes') {
-    alert('That must have sucked!');
-    //   console.log(poisonIvy);
-  }
-  else if (poisonIvy === 'no') {
-    alert('Same, we are so lucky');
-    // console.log(poisonIvy);
-  }
-  else {
-    alert('Try again');
-    // console.log(poisonIvy);
-
-  }
-}
-fourthYoN();
-
-
-function fifthYoN() {
-  let ready = prompt('Are you ready?');
-  ready = ready.toLowerCase();
-  console.log(ready);
-
-  if (ready === 'yes') {
-    alert("LET'S GO!");
-    //   console.log(ready);
-  }
-  else if (ready === 'no') {
-    alert('You better get ready');
-    // console.log(ready);
-  }
-  else {
-    alert('Ready is not your middle name');
-    // console.log(ready);
-
-  }
-}
-fifthYoN();
-
-
-
-
+let correct = false;
 for (let x = 1; x < 5; x++) {
-  let favMovie = prompt('How many times have I seen the Princess Bride');
-  favMovie = parseInt(favMovie);
-  if (favMovie < 100)
+  let favMovie = parseInt(prompt('How many times have I seen the Princess Bride?'));
+  if (favMovie < 100) {
     alert('Too low.');
-  else if (favMovie > 100)
+  } else if (favMovie > 100) {
     alert('Too high.');
-  else if (favMovie === 100)
+  } else {
     alert('Inconceivable!');
-  else {
-    alert('Try again');
+    correct = true;
+    break;
   }
-
 }
-alert('So close! but the answer is 100');
+if (!correct) {
+  alert('So close! but the answer is 100');
+}
+const pastaShapes = ['farfelle', 'penne', 'rigatoni', 'tortellini', 'cavatappi'];
 
-const pastaShapes = [];
-pastaShapes.push('farfelle', 'penne', 'rigatoni', 'tortellini', 'cavatappi');
-console.log(pastaShapes);
-
-let favPasta = prompt('Do we like the same types of pasta?');
-favPasta = favPasta.toLowerCase();
+let favPasta = prompt('Do we like the same types of pasta?').toLowerCase();
 
 let found = false;
 
-for (let a = 0; a < pastaShapes.length; a++) {
-  if (favPasta === pastaShapes[a]) {
-    alert('You know what they say about great minds...');
+for (let i = 0; i < pastaShapes.length; i++) {
+  if (favPasta === pastaShapes[i]) {
     found = true;
     break;
   }
-
-  if (!found) {
-    alert('Guess not :(');
-
-  
-  }
 }
 
-// // function doAnything() {
-// //   return "I will do this thing " + the Thing";
+if (found) {
+  alert('You know what they say about great minds...');
+} else {
+  alert('Guess not :(');
+}
 
-// }
+
+/*
+const pastaShapes = ['farfelle', 'penne', 'rigatoni', 'tortellini', 'cavatappi'];
+
+let favPasta = prompt('Do we like the same types of pasta?').toLowerCase();
+
+if (pastaShapes.includes(favPasta)) {
+  alert('You know what they say about great minds...');
+} else {
+  alert('Guess not :(');
+}
+*/
